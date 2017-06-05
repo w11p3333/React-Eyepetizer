8cc8bc97426edaaae6aedb2f1ba6956d202f20f1
w11p3333 <applecatkay@gmail.com>
2017-06-05 10:57:36 +0800

    fix devtool bug

--- src/components/common/modal/qrcode.js
+++ src/components/common/modal/qrcode.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const size = '300px'
+const size = 300
 export default styled.img`
-  height: ${size};
-  width: ${size};
+  height: ${size}px;
+  width: ${size}px;
 `
