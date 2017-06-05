c59316bad83dd33be045da3967724162fa318b6f
w11p3333 <applecatkay@gmail.com>
2017-06-05 11:50:40 +0800

    update example

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -72,13 +72,13 @@ module.exports = merge(baseConfig, {
       filename: 'css/[name].[contenthash].css'
     }),
     new SWPrecachePlugin({
-      cacheId: 'react-eyepetizer-version-1.6',
+      cacheId: 'react-eyepetizer-version-1.7',
       filename: 'service-worker.js',
       dontCacheBustUrlsMatching: /./,
       staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
       runtimeCaching: [
         {
-          urlPattern: '/^https:\/\/baobab.kaiyanapp\.com\/api/',
+          urlPattern: '/^https:\/\/baobab\.kaiyanapp\.com\/api/',
           handler: 'networkFirst'
         }
       ]
