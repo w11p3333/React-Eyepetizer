ba4af7ede4b31f084abd642077004f9544a66193
w11p3333 <applecatkay@gmail.com>
2017-06-25 21:04:24 +0800

    udpate config

--- src/components/detail/relateList/section.js
+++ src/components/detail/relateList/section.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
+import { mediaQuery } from '@/style'
 export default styled.section`
   margin-bottom: 20px;
   display: flex;
   height: 170px;
+
+  ${mediaQuery} {
+    height: 88px;
+  }
 `
--- src/components/home/banner/container.js
+++ src/components/home/banner/container.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export default styled.header`
   background-color: rgba(51, 51, 51, 1);
-  min-height: 100vh;
   position: relative;
   overflow: hidden;
   z-index: 0;
+  min-height: 100vh;
 `
